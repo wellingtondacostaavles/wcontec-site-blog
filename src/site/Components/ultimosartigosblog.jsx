ceeb76be9c56f788b696670b33d6aed2af85d87f
wellingtondacostaalves <contador.wellington@gmail.com>
2022-02-21 16:11:16 -0300

    Commit site de blog/ para blog-

--- src/site/Components/ultimosartigosblog.jsx
+++ src/site/Components/ultimosartigosblog.jsx
@@ -19,7 +19,7 @@ function Ultimosartigosblog(){
                             <div className="card-body">
                                 <h4 className="card-title">Já estão disponíveis as consultas ao Sistema Valores a Receber</h4>
                                 <p className="card-text">Consulte e saiba se você tem valor a receber ou não, caso tenha, ficará por dentro da data e o período para consultar e solicitar o resgate do saldo existente.</p>
-                                <a href="/blog/consulta-ao-sistema-valores-a-receber-bcb" className="btn btn-primary btn-artigos">Ler Post</a>
+                                <a href="/blog-consulta-ao-sistema-valores-a-receber-bcb" className="btn btn-primary btn-artigos">Ler Post</a>
                             </div>
                         </div>  
                     </div>
@@ -31,7 +31,7 @@ function Ultimosartigosblog(){
                         <div className="card-body">
                             <h4 className="card-title">Senado aprova tributação menor para caminhoneiros</h4>
                             <p className="card-text">O Senado aprovou um projeto de lei que muda a tributação de caminhoneiros autônomos inscritos como microempreendedores individuais.</p>
-                            <a href="/blog/senado-aprova-tributacao-menor-para-caminhoneiros" className="btn btn-primary btn-artigos">Ler Post</a>
+                            <a href="/blog-senado-aprova-tributacao-menor-para-caminhoneiros" className="btn btn-primary btn-artigos">Ler Post</a>
                         </div>
                     </div>  
                 </div>  
@@ -42,7 +42,7 @@ function Ultimosartigosblog(){
                         <div className="card-body">
                             <h4 className="card-title">Como aproveitar o contrato de experiência com o empregado?</h4>
                             <p className="card-text">Todas as contratações de empregados são feitas com um período de experiência para que o empregador possa avaliar se as aptidões do novo contratado.</p>
-                            <a href="/blog/como-aproveitar-ao-maximo-o-contrato-de-experiencia-de-trabalho-com-o-empregado" className="btn btn-primary btn-artigos">Ler Post</a>
+                            <a href="/blog-como-aproveitar-ao-maximo-o-contrato-de-experiencia-de-trabalho-com-o-empregado" className="btn btn-primary btn-artigos">Ler Post</a>
                         </div>
                     </div>  
                 </div>  
