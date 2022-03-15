104b26d928e0fa4b152703debee2248cd72eecd2
wellingtondacostaalves <contador.wellington@gmail.com>
2022-03-15 13:11:32 -0300

    Ajustes em ultimosartigosblog

--- src/site/Components/ultimosartigosblog.jsx
+++ src/site/Components/ultimosartigosblog.jsx
@@ -18,7 +18,7 @@ function Ultimosartigosblog(){
                         <center><img src="/Images/DIRPF22/Deducoeslegaisoudescontosimplificado/ultimosartigos.png" className="card-img-top" alt="..."/></center>
                         <div className="card-body">
                             <h4 className="card-title">Deduções legais ou desconto simplificado, qual escolher?</h4>
-                            <p className="card-text">Vai depender do total de despesas dedutíveis que você tem em relação à renda. Daí você pode avaliar se é mais vantajoso optar por deduções legais ou desconto simplificado.</p>
+                            <p className="card-text">Vai depender do total de despesas dedutíveis que você tem em relação à renda. Você pode avaliar se é melhor optar por deduções legais ou desconto simplificado.</p>
                             <a href="/blog-deducoes-legais-desconto-simplificado-irpf-22#" className="btn btn-primary btn-artigos">Ler Post</a>
                         </div>
                     </div>  
@@ -39,8 +39,8 @@ function Ultimosartigosblog(){
                     <div className="card card-artigos">
                         <center><img src="/Images/Novidadesnadeclaracaoirpf22/ultimosartigos.png" className="card-img-top" alt="..."/></center>
                         <div className="card-body">
-                            <h4 className="card-title">Confira as novidades na declaração do Imposto de Renda 2022</h4>
-                            <p className="card-text">São várias mudanças, e a que chama mais atenção será o recebimento da restituição ou o pagamento do imposto por meio de Pix.</p>
+                            <h4 className="card-title">IR - Confira as novidades na declração de 2022</h4>
+                            <p className="card-text">São várias mudanças, e a que chama mais atenção será o recebimento da restituição ou o pagamento do imposto por meio de Pix, basta a cheve ser o CPF.</p>
                             <a href="/blog-novidades-na-declaracao-irpf-22" className="btn btn-primary btn-artigos">Ler Post</a>
                         </div>
                     </div>  
