b708b3d6e84f5392e03c3cac46749cb88d130091
wellingtondacostaalves <contador.wellington@gmail.com>
2022-08-18 14:33:12 -0300

    Commit imagens notbook chamada e  ajustes inicio

--- src/site/Components/inicio.jsx
+++ src/site/Components/inicio.jsx
@@ -32,8 +32,7 @@ function Inicio(){
                     </div>
                 </div>                
                 <div className="col-lg-6">
-                    <h2 className="chamado-h2">Vamos gerenciar a saúde</h2>
-                    <h2 className="chamado-h2"> financeira da sua empresa daqui,</h2>   
+                    <h2 className="chamado-h2">Vamos cuidar da sua empresa daqui,</h2>
                     <div id="carouselExampleControls" className="carousel carousel-dark  slide" data-bs-ride="carousel">
                         <div className="carousel-inner">
                             <div className="carousel-item active" data-bs-interval="3000">    
@@ -48,15 +47,15 @@ function Inicio(){
                             <div className="carousel-item" data-bs-interval="1500">    
                                 <h2 className="chamado2-h2">colaborativamente</h2>
                             </div>
-                            <div className="carousel-item" data-bs-interval="900000">    
-                                <h2 className="chamado2-h2">juntos.<span className="chamado4-h2"> Com a Wcontec.</span></h2>
+                            <div className="carousel-item" data-bs-interval="33300">    
+                                <h2 className="chamado2-h2">juntos.<span className="chamado4-h2"> Wcontec.</span></h2>
                             </div>
                         </div>
                     </div>           
                     <p></p>
                     <h4 className="chamado3-h4">A plataforma de gestão financeira e assessoria contábil online.</h4>
-                    <a href="https://wcontec.osayk.com.br/#/cadastro" target="_blank" className="btn btn-success btn-lg btn-banner">Cadastre-se</a>
-                    <a href="https://wcontec.osayk.com.br/#/login" target="_blank" className="btn btn-outline-primary btn-lg btn-banner">Acessar</a>
+                    <a href="https://wcontec.osayk.com.br/#/cadastro" target="_blank" className="btn btn-success btn-lg btn-chamado">Cadastre-se</a>
+                    <a href="https://wcontec.osayk.com.br/#/login" target="_blank" className="btn btn-outline-primary btn-lg btn-chamado">Acessar</a>
                 </div>                        
             </div>    
         </div>
