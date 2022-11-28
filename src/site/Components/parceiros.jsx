553769e216b5157da5e5e88e25696778843cff64
wellingtondacostaalves <contador.wellington@gmail.com>
2022-11-28 14:36:23 -0300

    Commit parceiro serasa

--- src/site/Components/parceiros.jsx
+++ src/site/Components/parceiros.jsx
@@ -6,12 +6,14 @@ function Parceiros(){
         <div className="container">
             <div className="row coluna1">
                 <div className="col"></div>
-                <div className="col"></div>
+                <div className="col">
+                    <a href="https://api.whatsapp.com/send?phone=5581997780156" target="_blank"><img src="/Images/Parceiros/Diniz-contabilidade.png"/></a>
+                </div>
                 <div className="col">
                 <a href="https://imperio-dos-descartaveis.kyte.site/" target="_blank"><img src="/Images/Parceiros/ImperioDescartaveis.png"/></a>
                 </div>
                 <div className="col">
-                    <a href="https://api.whatsapp.com/send?phone=5581997780156" target="_blank"><img src="/Images/Parceiros/Diniz-contabilidade.png"/></a>
+                    <a href="https://www.instagram.com/invites/contact/?i=8xi8cgtzehge&utm_content=3egv8nw" target="_blank"><img src="/Images/Parceiros/Issoebom.png"/></a>
                 </div>
                 <div className="col">
                     <a href="https://go.hotmart.com/H67767543F?ap=305a" target="_blank"><img src="/Images/Parceiros/Vargas.png"/></a>
@@ -19,7 +21,9 @@ function Parceiros(){
                 <div className="col">
                     <a href="https://api.whatsapp.com/send?phone=5581998753986" target="_blank"><img src="/Images/Parceiros/OGS.png"/></a>
                 </div>
-                <div className="col"></div>
+                <div className="col">
+                    <a href="http://bit.ly/3ASk0Nm" target="_blank"><img src="/Images/Parceiros/Serasa.png"/></a>
+                </div>
                 <div className="col"></div>
             </div>
             <div className="row coluna2">
@@ -40,7 +44,7 @@ function Parceiros(){
                     <a href="https://github.com/" target="_blank"><img src="/Images/Parceiros/GitHub.png"/></a>
                 </div>
                 <div className="col">
-                    <a href="https://www.instagram.com/invites/contact/?i=8xi8cgtzehge&utm_content=3egv8nw" target="_blank"><img src="/Images/Parceiros/Issoebom.png"/></a>
+                    <a href="https://bit.ly/cadastre-se-na-wcontec" target="_blank"><img src="/Images/Parceiros/Torne-se-parceiro.png"/></a>
                 </div>
                 <div className="col"></div>
             </div>
