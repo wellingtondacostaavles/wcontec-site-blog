b72a624349f5d2988019232c8d2acdb112a86709
wellingtondacostaalves <contador.wellington@gmail.com>
2023-05-02 19:35:55 -0300

    Commit ajustes link markt. gestão de estoque nfe

--- src/link-marketing/getao-de-estoque-emissao-nfe/gestao-de-estoque-emissao-nfe.jsx
+++ src/link-marketing/getao-de-estoque-emissao-nfe/gestao-de-estoque-emissao-nfe.jsx
@@ -7,11 +7,11 @@ function GestaoNFE(){
       
     <section id="gestaoNFE">
       
-    <div className="container">
-		<div className="header">
+    <div className="container-gestaoNFE">
+		<div className="header-gestaoNFE">
 			<h1>Transforme seu negócio com uma <span class="segundo-h1">gestão de estoque</span> inteligente e ágil: aprenda a <span class="segundo-h1">emitir NF-e</span> e controlar movimentações de forma descomplicada!</h1>
 		</div>
-		<div className="content">
+		<div className="content-gestaoNFE">
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/9pTUws0N3OA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			<a href="https://bit.ly/3gkBvyJ" className="button">Confira aqui mais dicas como esta</a>
 		</div>
