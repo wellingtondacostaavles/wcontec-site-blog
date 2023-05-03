6ee40d5b16b455548690fa30abfffb7234679238
wellingtondacostaalves <contador.wellington@gmail.com>
2023-05-03 15:41:54 -0300

    Commit ajustes em gestaoNFE

--- src/link-marketing/getao-de-estoque-emissao-nfe/gestao-de-estoque-emissao-nfe.jsx
+++ src/link-marketing/getao-de-estoque-emissao-nfe/gestao-de-estoque-emissao-nfe.jsx
@@ -9,19 +9,19 @@ function GestaoNFE(){
       
     <div className="container-gestaoNFE">
 		<div className="header-gestaoNFE">
-			<h1>Transforme seu negócio com uma <span class="segundo-h1">gestão de estoque</span> inteligente e ágil: aprenda a <span class="segundo-h1">emitir NF-e</span> e controlar movimentações de forma descomplicada!</h1>
+			<h1>Transforme seu negócio com uma <span className="segundo-h1">gestão de estoque</span> inteligente e ágil: aprenda a <span class="segundo-h1">emitir NF-e</span> e controlar movimentações de forma descomplicada!</h1>
 		</div>
-		<div className="content-gestaoNFE">
+		<div className="content-gestaoNFE text-center">
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/9pTUws0N3OA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-			<a href="https://bit.ly/3gkBvyJ" className="button">Confira aqui mais dicas como esta</a>
+			<a href="https://bit.ly/3gkBvyJ" className="btn btn-primary btn-lg btn-gestaoNFE">Confira aqui mais dicas como esta</a>
 		</div>
 		<footer className="social-media">
-			<a href="https://www.facebook.com/profile.php?id=100072384950030" target="_blank"><i class="fab fa-facebook-square"></i></a>
-			<a href="https://instagram.com/wcontec?r=nametag" target="_blank"><i class="fab fa-instagram-square"></i></a>
-			<a href="https://youtube.com/@wcontec2017" target="_blank"><i class="fab fa-youtube-square"></i></a>
-			<a href="https://twitter.com/w_contec" target="_blank"><i class="fab fa-twitter-square"></i></a>
-			<p>Todos os direitos reservados © 2023</p>
-		</footer>		  
+			<a href="https://www.facebook.com/profile.php?id=100072384950030" target="_blank"><i className="fab fa-facebook-square"></i></a>
+			<a href="https://instagram.com/wcontec?r=nametag" target="_blank"><i className="fab fa-instagram-square"></i></a>
+			<a href="https://youtube.com/@wcontec2017" target="_blank"><i className="fab fa-youtube-square"></i></a>
+			<a href="https://twitter.com/w_contec" target="_blank"><i className="fab fa-twitter-square"></i></a>
+			<p>Copyright © 2023 Wcontec. Todos direitos reservados</p>
+		</footer>	  
 	</div>
 
     </section>            
