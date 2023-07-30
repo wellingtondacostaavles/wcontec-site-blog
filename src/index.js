d2b53f681b6c1df10d07520a3aa548baa3c183ba
wellingtondacostaalves <contador.wellington@gmail.com>
2023-07-30 19:58:00 -0300

    Commit ajustes preloader

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import {AuthProvider} from './app/Context/auth';
 import AppWithPreloader from './preloader/AppWithPreloader';
 
-ReactDOM.render(
-<AuthProvider>
-    <AppWithPreloader/>
-</AuthProvider>, 
-document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDOM.render(
+      <AuthProvider>
+        <AppWithPreloader />
+      </AuthProvider>,
+      document.getElementById('root')
+    );
+  });
+  
+  
+  
--- src/preloader/preloader.jsx
+++ src/preloader/preloader.jsx
@@ -1,26 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import './preloader.css';
 
 const Preloader = () => {
   const colors = ['blue', 'orange', 'green'];
-  const [currentColorIndex, setCurrentColorIndex] = useState(0); // Começar com o índice 0 para a cor azul
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const [pageLoaded, setPageLoaded] = useState(false); // Estado para controlar o carregamento da página
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
     }, 500);
 
-    return () => clearInterval(interval);
-  }, []);
-  
+    // Se a página ainda não estiver carregada, adicionamos um ouvinte para o evento `load`.
+    // Quando a página estiver totalmente carregada, chamamos a função `handlePageLoad`.
+    if (!pageLoaded) {
+      window.addEventListener('load', handlePageLoad);
+    }
 
-    return (
-      <div className="preloader-wrapper">
-        <div className={`spinner-border text-${colors[currentColorIndex]}`} role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      </div>
-    );
+    return () => {
+      clearInterval(interval);
+      // Removemos o ouvinte do evento `load` quando o componente é desmontado.
+      window.removeEventListener('load', handlePageLoad);
+    };
+  }, [pageLoaded]);
+
+  // Função para ser executada quando a página estiver totalmente carregada.
+  const handlePageLoad = () => {
+    setPageLoaded(true);
   };
+
+  return (
+    <div className={`preloader-wrapper${pageLoaded ? ' fade-out-animation' : ''}`}>
+      <div className={`spinner-border text-${colors[currentColorIndex]}`} role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
 export default Preloader;
