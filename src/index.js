72ec6cec551752a2d7de92154b751da04b1a0296
wellingtondacostaalves <contador.wellington@gmail.com>
2023-08-21 18:57:27 -0300

    Commit tirando index.js tirando o AppWithPreloader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AuthProvider } from './app/Context/auth';
-import AppWithPreloader from './preloader/AppWithPreloader';
+import App from './app';
 
 ReactDOM.render(
   <AuthProvider>
-    <AppWithPreloader />
+    <App />
   </AuthProvider>,
   document.getElementById('root')
 );
