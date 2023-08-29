9d51b7509c8e0f8685da2cc172805c3a9be875c6
wellingtondacostaalves <contador.wellington@gmail.com>
2023-08-29 13:08:07 -0300

    Commit ajustes parceiros

--- src/site/Components/parceiros.jsx
+++ src/site/Components/parceiros.jsx
@@ -3,53 +3,97 @@ import react from "react";
 function Parceiros(){
     return <section id="parceiros">   
         <div className="parceiros-logos">  
-        <div className="container">
-            <div className="row coluna1">
-                <div className="col"></div>
-                <div className="col">
-                    <a href="https://api.whatsapp.com/send?phone=5581997780156" target="_blank"><img src="/Images/Parceiros/Diniz-contabilidade.png"/></a>
+            <div className="container">
+                <div className="row primeira-linha">
+                    <div className="col"></div>
+                    <div className="col"></div>
+                    <div className="col grayscale-image">
+                        <a href="https://www.instagram.com/arouchagas.supergasbras/" target="_blank">
+                            <img src="/Images/Parceiros/ArouchaGas.png" />
+                        </a>
+                    </div>
+                    <div className="col grayscale-image">
+                        <a href="https://api.whatsapp.com/send?phone=5581988823062" target="_blank">
+                            <img src="/Images/Parceiros/JH.png" />
+                        </a>
+                    </div>
+                    <div className="col grayscale-image">
+                        <a href="http://bit.ly/3ASk0Nm" target="_blank">
+                            <img src="/Images/Parceiros/Serasa.png" />
+                        </a>
+                    </div>
+                    <div className="col grayscale-image">
+                        <a href="https://bit.ly/cadastre-se-na-wcontec" target="_blank">
+                            <img src="/Images/Parceiros/Torne-se-parceiro.png" />
+                        </a>
+                    </div>
+                    <div className="col"></div>
+                    <div className="col"></div>
                 </div>
-                <div className="col">
-                <a href="https://imperio-dos-descartaveis.kyte.site/" target="_blank"><img src="/Images/Parceiros/ImperioDescartaveis.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://www.instagram.com/invites/contact/?i=8xi8cgtzehge&utm_content=3egv8nw" target="_blank"><img src="/Images/Parceiros/Issoebom.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://go.hotmart.com/H67767543F?ap=305a" target="_blank"><img src="/Images/Parceiros/Vargas.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://api.whatsapp.com/send?phone=5581998753986" target="_blank"><img src="/Images/Parceiros/OGS.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="http://bit.ly/3ASk0Nm" target="_blank"><img src="/Images/Parceiros/Serasa.png"/></a>
-                </div>
-                <div className="col"></div>
+                <div className="row linhas-meio">
+                    <div className="col"></div>                
+                    <div className="col grayscale-image">
+                        <a href="https://imperio-dos-descartaveis.kyte.site/" target="_blank">
+                            <img src="/Images/Parceiros/ImperioDescartaveis.png" />
+                        </a>
+                    </div>
+                    <div className="col grayscale-image">
+                        <a href="https://www.instagram.com/invites/contact/?i=8xi8cgtzehge&utm_content=3egv8nw" target="_blank">
+                            <img src="/Images/Parceiros/Issoebom.png" />
+                        </a>
+                    </div>
+                    <div className="col-3 grayscale-image">
+                        <a href="https://www.instagram.com/gomesdefigueiredo.adv/#" target="_blank">
+                            <img src="/Images/Parceiros/Gomes-Figueredo.png" />
+                        </a>
+                    </div>
+                    <div className="col grayscale-image">
+                        <a href="https://api.whatsapp.com/send?phone=5581998753986" target="_blank">
+                            <img src="/Images/Parceiros/OGS.png" />
+                        </a>
+                    </div>
+                    <div className="col"></div>
             </div>
-            <div className="row coluna2">
-                <div className="col"></div>
-                <div className="col">
-                    <a href="https://aws.amazon.com/pt/" target="_blank"><img src="/Images/Parceiros/AWS.png"/></a>
+                
+                <div className="row ultima-linha">
+                    <div className="col-2 grayscale-image">
+                    <a href="https://api.whatsapp.com/send?phone=5581997780156" target="_blank">
+                        <img src="/Images/Parceiros/Diniz-contabilidade.png" />
+                    </a>
+                    </div>
+                    <div className="col grayscale-image">
+                    <a href="https://api.whatsapp.com/send?phone=5581988179652" target="_blank">
+                        <img src="/Images/Parceiros/Sintel.png" />
+                    </a>
+                    </div>
+                    <div className="col grayscale-image">
+                    <a href="/parceiro-certificacao-digital" target="_blank">
+                        <img src="/Images/Parceiros/Certificado.digital.png" />
+                    </a>
+                    </div>
+                    <div className="col grayscale-image">
+                    <a href="https://osayk.com.br/" target="_blank">
+                        <img src="/Images/Parceiros/Osayk.png" />
+                    </a>
+                    </div>
+                    <div className="col grayscale-image">
+                    <a href="https://github.com/" target="_blank">
+                        <img src="/Images/Parceiros/GitHub.png" />
+                    </a>
+                    </div>
+                    <div className="col grayscale-image">
+                    <a href="https://aws.amazon.com/pt/" target="_blank">
+                        <img src="/Images/Parceiros/AWS.png" />
+                    </a>    
+                    </div>
+                    <div className="col grayscale-image">
+                    <a href="https://go.hotmart.com/H67767543F?ap=305a" target="_blank">
+                        <img src="/Images/Parceiros/Vargas.png" />
+                    </a>
+                    </div>
                 </div>
-                <div className="col">
-                    <a href="/parceiro-certificacao-digital" target="_blank"><img src="/Images/Parceiros/Certificado.digital.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://osayk.com.br/" target="_blank"><img src="/Images/Parceiros/Osayk.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://jhmateriaisparaconstrucao.com/" target="_blank"><img src="/Images/Parceiros/JH.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://github.com/" target="_blank"><img src="/Images/Parceiros/GitHub.png"/></a>
-                </div>
-                <div className="col">
-                    <a href="https://www.instagram.com/arouchagas.supergasbras/" target="_blank"><img src="/Images/Parceiros/ArouchaGas.png"/></a>
-                </div>
-                <div className="col"></div>
             </div>
-        </div>
-    </div>    
+        </div>    
     <div className="container-fluid">
         <div className="row">
                 <p className="position-parceiros-p">Parceiros</p>             
