408b03127a78e4bea23c0bc71b65a8dc81cb7106
wellingtondacostaalves <contador.wellington@gmail.com>
2023-09-06 18:49:53 -0300

    Commit Imagens Banners

---
+++ src/Banners/banners.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SeuComponente() {
+  return (
+    <div>
+      <img src="/images/Banners/Banner-160x600.png" alt="Banner do projeto" />
+      <img src="/images/Banners/Banner-250x250.png" alt="Banner do projeto" />
+      <img src="/images/Banners/Banner-300x250.png" alt="Banner do projeto" />
+      <img src="/images/Banners/Banner-336x280.png" alt="Banner do projeto" />
+      <img src="/images/Banners/Banner-468x60.png" alt="Banner do projeto" />
+      <img src="/images/Banners/Banner-728x90.png" alt="Banner do projeto" />
+    </div>
+  );
+}
+
+export default SeuComponente;
